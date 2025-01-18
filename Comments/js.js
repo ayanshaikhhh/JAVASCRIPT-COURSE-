80be3996106cdaf78aef52740ea63a2164049002
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-01-18 11:48:56 +0530

    comment update

---
+++ Comments/js.js
@@ -0,0 +1,13 @@
+document.write("Hello World <br>"); // here is comment
+
+// document.write("Hello World"); 
+
+
+
+
+
+
+/*
+document.write("Hello World <br>");
+document.write("Hello World"); 
+*/
