659ebc599d402ca2e4becc70088668bed0bdbcfa
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-01-19 13:09:01 +0530

    Arithmetic Operators

---
+++ 7) Arithmetic Operators/js.js
@@ -0,0 +1,91 @@
+/*  
+Operators                     Description
+    +                         Addition
+    -                         Subtration
+    *                         Multiplication  
+    **                        Exponentiation
+    /                         Division
+    %                         Modulus (Remainder)
+    ++                        Increment
+    --                        Decrement
+*/
+
+
+
+let a = 7;
+let b = 5;
+let c = a + b;
+document.write(c);
+
+
+let d = 3;
+let e = 2;
+let f = d - e;
+document.write("<br>", f);
+
+
+let g = 5;
+let h = 5;
+let i = g * h;
+document.write("<br>", i);
+
+
+let j = 10;
+let k = 3;
+let l = j ** k;
+document.write("<br>", l);
+
+
+let m = 14;
+let n = 2;
+let o = m / n;
+document.write("<br>", o);
+
+
+let p = 14;
+let q = 2;
+let r = p % q;
+document.write("<br>", r);
+
+
+let s = 50;
+s++;
+document.write("<br>", s);
+let t = 12;
+let u = s + t;
+document.write("<br>",u );
+
+
+
+let v = 65;
+v--;
+document.write("<br>", v);
+let w = 4;
+let x = v - w;
+document.write("<br>", x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
