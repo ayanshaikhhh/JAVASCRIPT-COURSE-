b0f0ba35c46ea3c3a145bd07352df4dc4d5f7010
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-01-19 12:31:31 +0530

    Data Types

---
+++ 6) Data Types/js.js
@@ -0,0 +1,47 @@
+/*
+=> Different Types of Data Types.
+
+var x = "Hello World";------------------------>String
+var x = 25;----------------------------------->Number
+var x = true;--------------------------------->Boolean
+var x = ["HTML","CSS","JS"];------------------>Array
+var x = {first:"jan", second:"feb"};---------->Object
+var x = null;--------------------------------->Null
+var x;---------------------------------------->Undefined
+
+*/
+
+let x = "Hello World";
+let y = 123;
+let z = true;
+let a;
+let b = null;
+let w = ["html","css","js"];
+let o = {
+    name: "macbook",
+    class: "six",
+    rollNo: "123",
+};
+
+
+
+document.write(x);
+document.write("<br>", typeof x);  //typeof ek function hai.
+document.write("<br>", typeof y);  // number
+document.write("<br>", typeof z);  // boolean
+document.write("<br>", typeof w);  //array
+document.write("<br>", typeof a);  
+document.write("<br>", typeof b);  
+document.write("<br>", typeof o);  
+
+
+
+
+
+
+
+
+
+
+
+
