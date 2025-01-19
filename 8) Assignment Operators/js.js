ab4a534e9a240a97d8a3de6210a331b8d51636ce
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-01-19 14:10:38 +0530

    Assignment Operators

---
+++ 8) Assignment Operators/js.js
@@ -0,0 +1,63 @@
+/*
+Operators                   Example                     Same As
+    =                       x = y                       x = y
+    +=                      x += y                      x = x + y
+    -=                      x -= y                      x = x - y
+    *=                      x *= y                      x = x * y
+    /=                      x /= y                      x = x / y
+    %=                      x %= y                      x = x % y
+    **=                     x **= y                     x = x ** y
+*/
+
+
+
+let a = 10;   // Let Variable ke andar ham value ko  override kr sakte hai New Value Assign kr sakte hai.
+let b = 20;   // Let Variable ko dobara declare nahi kr sakte.
+b = 10;
+a +=b;
+document.write(a);
+
+
+let c = 12;
+let d = 11;
+c -= d;
+document.write("<br>", c);
+
+
+let e = 3;
+let f = 4;
+e *= f;
+document.write("<br>", e);
+
+
+let g = 14;
+let h = 2;
+g /= h;
+document.write("<br>", g);
+
+
+let i = 50;
+let j = 2;
+i %= j;
+document.write("<br>", i);
+
+
+let k = 10;
+let l = 5;
+k **= l;
+document.write("<br>", k);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
