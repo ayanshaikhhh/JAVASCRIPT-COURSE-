2d07cadf48eac6d9151af628d6f36780cad1193a
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-01-20 19:31:53 +0530

    Google Chrome Console

---
+++ 9) Google Chrome Console/js.js
@@ -0,0 +1,18 @@
+x = 10;
+console.log(x);
+console.table([1,2,3,4]); 
+console.error("heyy");
+console.warn("this is a JS ")
+console.clear();
+
+console.time("hey");
+console.timeEnd("hey");
+
+
+
+
+
+
+
+
+
