30e7dd9c67540d2712e2be87859a45b37f57bce2
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-01-22 15:54:57 +0530

    Comparision Operator

---
+++ 10) Comparision Operators/js.js
@@ -0,0 +1,48 @@
+/*
+Operator                       Description
+   ==                          equal to
+   ===                         equal value and equal data type
+   !=                          not equal
+   !==                         not equal value or not equal data type
+   >                           greater than
+   >=                          greater than or equal to
+   <                           less than
+   <=                          less than or equal to
+*/
+ 
+
+let a = 16;
+let b = "10";
+let c = a == b;
+let d = a === b;
+let e = a != b;
+let f = a !== b;
+let g = a > b;
+let h = a < b;
+let i = a >= b;
+let j = a <= b;
+
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
