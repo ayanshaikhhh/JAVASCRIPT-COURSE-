fb2ac9c1494c59255ef9acb053f594dda9244501
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-01-28 17:37:17 +0530

    Conditionary Ternary Operator

---
+++ 15) Conditional Ternary Operator/js.js
@@ -0,0 +1,38 @@
+let a = 10;
+let b;
+(a == 100)? b="true" : b="false";
+document.write(b, "<br>")
+
+
+
+let x = 50;
+let y;
+y = "Value" + (x>60 ? y=" X Is Greater" : y=" X Is Smallar");
+document.write(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
