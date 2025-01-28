c98230ab722d8497838e0b42a5d914091c95b04a
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-01-28 14:29:17 +0530

    If Else If  Statement

---
+++ 14) If Else If Statement/js.js
@@ -0,0 +1,44 @@
+/*
+let time = prompt("Enter Time");
+
+if (time < "10") {
+    document.write("Good Morning");
+} else if (time < 20) {
+    document.write("Good Day");
+} else {
+    document.write("Good Evening");
+}
+*/
+
+
+
+
+
+let per = prompt("Enter Yuor Percentage");
+
+if (per>=80 && per<=100) {
+    document.write("You are in Merit")
+} else if (per>=60 && per<= 79) {
+    document.write("you are 1st division")
+} else if (per >=45 && per <= 59) {
+    document.write("You are 2nd division");
+}  else if (per >= 33 && per <= 44) {
+    document.write("you are 3rd division")
+} else if (per < 33) {
+    document.write("You are fail");
+} else {
+    document.write("Please Enter Valid Percentage");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
