e27cf5134b0eda44a199396e25fbf6d922924d58
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-01-28 11:41:51 +0530

    If Else Statement

---
+++ 13) If Else Statement/js.js
@@ -0,0 +1,43 @@
+let x = 22;
+let y = 20;
+if(x>y) {
+    document.write("X is greater <br>");
+} else {
+    document.write("Y is greater <br>")
+}
+
+
+
+
+
+// let name = "Sahil";
+// let gender = "Male";
+// if (gender == "Male") {
+//     document.write("Hello Mr." + name);
+// } else {
+//     document.write("Hello Miss." + name);
+// }
+
+
+
+
+let name = prompt("Enter your name");
+let gender = prompt("Enter your gender");
+
+if(gender==="male") {
+    document.write("Heyy Mr." + name);
+} else {
+    document.write("Heyy miss." + name);
+}
+
+
+
+
+
+
+
+
+
+
+
+
