39e9a561d6837466647725df478f46e62267cf56
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-02-04 10:45:35 +0530

    Functions

---
+++ 20) Functions/js.js
@@ -0,0 +1,49 @@
+/*
+function age() {
+    document.write("21");
+}
+age();
+*/
+
+
+
+
+/*
+function age() {
+    let age = prompt("Enter a age");
+    document.write(age);
+} 
+
+age();
+*/
+
+
+/*
+function name(){
+    let name = prompt("Enter the name");
+    document.write(name);
+    if(name==="") {
+        alert("Enter valid name");
+    } else{
+        document.write(name);
+    }
+}
+name();
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
