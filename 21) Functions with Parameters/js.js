b301868c488b9c68b4461e0c6277f94d23920c2b
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-02-07 11:33:38 +0530

    Function With Parameters

---
+++ 21) Functions with Parameters/js.js
@@ -0,0 +1,35 @@
+/*
+function hello(fname,lname) {
+    document.write("Hello dear, " + fname + lname);
+}
+hello("mba" , " mehndi wala");
+
+*/
+
+
+function name(fname=" sahil",lsname=" sindhi") {
+    document.write("Heyyy dear" + fname + lsname );
+}
+// name(" imran"," khan")
+// name(" mastani"," raja")
+name();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
