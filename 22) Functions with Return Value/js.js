048f8a70e24c7c3ffc30c2377d556c6a53f8293c
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-02-09 10:57:59 +0530

    Function with Return Value

---
+++ 22) Functions with Return Value/js.js
@@ -0,0 +1,59 @@
+/*
+function hello(a,b) {
+    let tt = a + b;
+    return tt;
+}
+
+let a = hello(5,5);
+document.write(a)
+*/
+
+
+
+
+/*
+function kid(name, age) {
+    let kid = name + "it is a " +  age + " years old";
+    return kid;
+}
+let prgmsuc = kid("talim ", 22);
+document.write(prgmsuc);
+*/
+
+
+
+function sum(java,mmt,english) {
+    let total = java + mmt + english;
+    return total;
+}
+
+
+function per(tm) {
+    let totalMarks = tm/300*100;
+    document.write(totalMarks);
+}
+
+
+let tt = sum(89,78,87);
+per(tt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
