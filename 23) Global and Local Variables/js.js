cca46e646b7e3fc320065dfa144da57e21a023a1
ayanshaikhhh <mohammedayan71636@gmail.com>
2025-02-09 11:35:47 +0530

    Global and Local Variables

---
+++ 23) Global and Local Variables/js.js
@@ -0,0 +1,49 @@
+
+let x = 10;  // This is a Globel Variable. isko ham function ke andar or function ke bahar kahi bhii use kr sakte hai..
+document.write(x, "<br>")
+
+
+
+
+
+
+function name() {
+    let name = "Sahil";  // This is a Local Variable. isko ham sirf function ke andar hi use kr sakte hai.. Function ke bahar use nahi kr sakte haii..
+    document.write(name)
+}
+name();
+
+
+
+
+/* This is a wrong mathod javascript isko support nahi krta....
+function x() {
+    let y = "Sahil";  // This is a Local Variable. isko ham sirf function ke andar hi use kr sakte hai.. Function ke bahar use nahi kr sakte haii..
+}
+document.write(y)
+x();
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
